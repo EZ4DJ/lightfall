b2feebf272a372b4494cf2e8b82c7904bbe0491c
silentblackcat <60523363+silentblackcat@users.noreply.github.com>
2024-12-06 01:29:30 -0500

    more progress

--- lightfall/lightfall.cpp
+++ lightfall/lightfall.cpp
@@ -1,87 +1,58 @@
 // LIGHTFALL: an EZ2AC Final EX score tracking hook
 // 
 
-#include <thread>
 #include "lightfall.h"
 #include "MinHook.h"
 
 #pragma comment(lib, "MinHook.x86.lib")
 
-typedef struct scoreInfo {
-	char title[128];
-	uint32_t mode;
-	char difficulty[4];
-	char course_name[128];
-	uint32_t level;
-	uint32_t score;
-	uint32_t rate;
-	char grade[5];
-	uint32_t total_notes;
-	uint32_t kool;
-	uint32_t cool;
-	uint32_t good;
-	uint32_t miss;
-	uint32_t fail;
-	uint32_t max_combo;
-};
+uint32_t* scoreArrayAddr;
 
-uint32_t* scoreArrayOffset;
-
+// stdcall to clean up stack from within the function, don't have to do it in asm
 void __stdcall getResultsScreenData() {
-	logger.logByteArray(scoreArrayOffset, 15);
+	logger.logByteArray(scoreArrayAddr, 15);
 
 }
 
-uintptr_t resScreenJumpLoc = 0x053A85;
-uintptr_t resScreenJumpBackLoc = resScreenJumpLoc + 5;
 __declspec(naked) void resScreenDetour() {
 	__asm {
-		mov scoreArrayOffset, edi; // edi has pointer we want
+		mov scoreArrayAddr, edi; // edi has pointer we want
 		pushad;
 		pushfd;
 		call getResultsScreenData;
 		popfd;
 		popad;
-		mov eax, DWORD PTR [0x1B2E548]; // the instruction that was replaced
+		mov eax, DWORD PTR [0x1B2E548]; // instruction that was replaced
 		jmp resScreenJumpBackLoc;
 	}
 }
 
-DWORD LightfallThread() {
-	logger.log("omg yippee");
-	LPCSTR twoezconfig = ".\\2EZ.ini";
-	int gameVer = GetPrivateProfileIntA("Settings", "GameVer", 0, twoezconfig);
-	if (gameVer != 21) {
-		logger.log("Game version not Final: EX, stopping");
-		return NULL;
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved) {
+	switch (ul_reason_for_call) {
+	case DLL_PROCESS_ATTACH: {
+		logger.log("omg yippee");
+		LPCSTR twoezconfig = ".\\2EZ.ini";
+		int gameVer = GetPrivateProfileIntA("Settings", "GameVer", 0, twoezconfig);
+		if (gameVer != 21) {
+			logger.log("Game version not Final: EX, stopping");
+			return NULL;
+		}
+
+		//Short sleep to fix crash when using legitimate data with dongle, can be overriden in ini if causing issues.
+		Sleep(GetPrivateProfileIntA("Settings", "ShimDelay", 10, twoezconfig));
+
+		// Parsing the results screen memory is easiest by inserting a hook in the middle of the function, where a register temporarily has an important pointer to score values
+		// Something like MinHook I don't think really works here, so I manually patching in the jump instruction
+		uint32_t relResScreenDetourAddr = (uint32_t)((LPBYTE)resScreenDetour - (baseAddress + resScreenJumpOffset + 5));
+
+		unsigned long OldProtection;
+		VirtualProtect((LPVOID)(baseAddress + resScreenJumpOffset), 5, PAGE_EXECUTE_READWRITE, &OldProtection);
+		*(BYTE*)(baseAddress + resScreenJumpOffset) = 0xE8; // jump opcode
+		memcpy((LPVOID)(baseAddress + resScreenJumpOffset + 1), (LPVOID)&relResScreenDetourAddr, 4);
+		VirtualProtect((LPVOID)(baseAddress + resScreenJumpOffset + 1), 5, OldProtection, NULL);
+
+		break;
 	}
-
-	//Short sleep to fix crash when using legitimate data with dongle, can be overriden in ini if causing issues.
-	Sleep(GetPrivateProfileIntA("Settings", "ShimDelay", 10, twoezconfig));
-
-	// Parsing the results screen memory is easiest by inserting a hook in the middle of the function, where a register temporarily has an important pointer to score values
-	// Tried using MinHook but couldn't get it to do what I needed, so I overwrote 5 bytes with a jump
-
-	uint32_t relResScreenDetourAddr = (uint32_t)((LPBYTE)resScreenDetour - (baseAddress + resScreenJumpLoc + 5));
-
-	unsigned long OldProtection;
-	VirtualProtect((LPVOID)(baseAddress + resScreenJumpLoc), 5, PAGE_EXECUTE_READWRITE, &OldProtection);
-	*(BYTE*)(baseAddress + resScreenJumpLoc) = 0xE8; // jump opcode
-	memcpy((LPVOID)(baseAddress + resScreenJumpLoc + 1), (LPVOID)&relResScreenDetourAddr, 4);
-	VirtualProtect((LPVOID)(baseAddress + resScreenJumpLoc + 1), 5, OldProtection, NULL);
-
-	return NULL;
-}
-
-BOOL APIENTRY DllMain(HMODULE hModule,
-	DWORD  ul_reason_for_call,
-	LPVOID lpReserved
-)
-{
-	switch (ul_reason_for_call)
-	{
-	case DLL_PROCESS_ATTACH:
-		CreateThread(0, 0, (LPTHREAD_START_ROUTINE)LightfallThread, 0, 0, 0);
 	case DLL_THREAD_ATTACH:
 	case DLL_THREAD_DETACH:
 	case DLL_PROCESS_DETACH:
--- lightfall/lightfall.h
+++ lightfall/lightfall.h
@@ -5,6 +5,32 @@
 Logger logger("lightfall.log");
 
 uintptr_t baseAddress = (uintptr_t)GetModuleHandleA(NULL);
+constexpr uintptr_t discNameAddr = 0x1B2EC18;
+constexpr uintptr_t modeAddr = 0x1B2E548;
+constexpr uintptr_t courseNameAddr = 0x1B2E728;
+constexpr uintptr_t levelAddr = 0x1B2EC0C;
+constexpr uintptr_t inCV2 = 0x1B2EB6C;
+
+constexpr uintptr_t resScreenJumpOffset = 0x053A85;
+uintptr_t resScreenJumpBackAddr = baseAddress + resScreenJumpOffset + 5;
+
+struct scoreInfo {
+	char title[128];
+	uint32_t mode;
+	char difficulty[4];
+	char course_name[128];
+	uint32_t level;
+	uint32_t score;
+	uint32_t rate;
+	char grade[5];
+	uint32_t total_notes;
+	uint32_t kool;
+	uint32_t cool;
+	uint32_t good;
+	uint32_t miss;
+	uint32_t fail;
+	uint32_t max_combo;
+};
 
 void readString(uintptr_t offset, char buffer[], size_t size) {
 	unsigned long OldProtection;
